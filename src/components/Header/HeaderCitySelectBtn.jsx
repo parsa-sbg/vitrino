86250a2f0d3724f25a17e74281e332a5bc56a9bb
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-16 21:51:02 +0330

    improve header responsive

--- src/components/Header/HeaderCitySelectBtn.jsx
+++ src/components/Header/HeaderCitySelectBtn.jsx
@@ -3,7 +3,7 @@ import { SlLocationPin } from "react-icons/sl";
 export default function HeaderCitySelectBtn() {
     return (
         <button
-            className="relative py-2 px-4 text-nowrap gap-2 rounded-l-lg md:rounded-lg flex items-center h-full transition-colors duration-300 border-l-2 border-y-2 md:border-none md:hover:bg-gray-100
+            className="relative py-2 px-4 text-nowrap gap-2 rounded-l-lg md:rounded-lg flex flex-row-reverse md:flex-row items-center h-full transition-colors duration-300 border-l-2 border-y-2 md:border-none md:hover:bg-gray-100
             before:h-7 before:w-px before:absolute before:right-0 before:bg-gray-300 md:before:hidden
         ">
             <SlLocationPin />
