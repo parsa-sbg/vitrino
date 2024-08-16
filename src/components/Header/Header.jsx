5b86349d242bbd7c52f7f7c5571fba32c5ba6708
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-16 21:34:02 +0330

    Make header responsive for mobile devices

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import Logo from "../Logo"
-import { SlLocationPin } from "react-icons/sl";
+import HeaderCitySelectBtn from "./HeaderCitySelectBtn";
 import HeaderCategories from "./HeaderCategories";
 import HeaderSearchBar from "./HeaderSearchBar";
 import HeaderMyVitrin from "./HeaderMyVitrin";
@@ -8,23 +8,25 @@ import SiteBtn from "../SiteBtn";
 
 export default function Header() {
     return (
-        <div className="border-b-2">
+        <div className=" md:border-b-2">
             <div className="container flex py-3 gap-4 justify-between items-center">
 
-                <div className="flex gap-2 items-center">
-                    <div className="w-52 border-l-2 pl-5"><Logo /></div>
+                {/* header right */}
+                <div className="w-full flex flex-row-reverse md:flex-row gap-0 md:gap-2 items-center">
 
-                    <button className="py-2 px-4 text-nowrap gap-2 rounded-lg flex items-center h-full transition-colors duration-300 hover:bg-gray-100">
-                        <SlLocationPin />
-                        ۱۳ شهر
-                    </button>
+                    <div className="hidden md:block w-52 border-l-2 pl-5"><Logo /></div>
 
-                    <HeaderCategories />
+                    <div> <HeaderCitySelectBtn /> </div>
+                    
 
-                    <HeaderSearchBar />
+                    <div className="hidden md:block"> <HeaderCategories /> </div>
+
+                    <div className="w-full md:max-w-96"> <HeaderSearchBar /> </div>
+
                 </div>
 
-                <div className="flex gap-2">
+                {/* header left */}
+                <div className="gap-2 hidden md:flex">
                     <HeaderMyVitrin />
                     <div className="">
                         <SiteBtn text={'ثبت آگهی'} />
