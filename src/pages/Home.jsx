b21f0371966668c0d609c5cd245c806c5b866967
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-20 00:14:15 +0330

    fix Home page style problems

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,15 +7,11 @@ import { FaLinkedin } from "react-icons/fa6";
 import { SiAparat } from "react-icons/si";
 
 
-
-
-
-
 export default function Home() {
   return (
 
 
-    <div className="container flex flex-col items-center gap-5 sm:gap-10 max-w-[566px] mx-auto mb-5 mt-4 md:mt-10">
+    <div className="container flex flex-col items-center gap-5 sm:gap-10 max-w-[566px] mx-auto pb-5 pt-4 md:pt-10">
 
 
       <div className=""><Logo /></div>
@@ -36,7 +32,7 @@ export default function Home() {
 
       {/* search city */}
       <div className="relative w-full">
-        <input className="border-2 outline-none focus:border-main hover:border-2 transition-colors border-gray-200 rounded-md py-2 pl-3 pr-10 w-full" placeholder="جستجوی شهر" type="text" />
+        <input className="border-2 outline-none dark:bg-transparent dark:border-gray-500 focus:border-main hover:border-2 transition-colors border-gray-200 rounded-md py-2 pl-3 pr-10 w-full" placeholder="جستجوی شهر" type="text" />
         <BsSearch className="absolute right-2 top-0 bottom-0 my-auto" size={20} />
       </div>
 
@@ -45,16 +41,16 @@ export default function Home() {
       <div className="w-full text-sm border-b-2 pb-4">
         <h4 className="font-bold">شهرهای پربازدید</h4>
         <div className="grid grid-cols-6 sm:grid-cols-5 mt-10 gap-y-16">
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 sm:col-span-1" >تهران</Link>
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 sm:col-span-1" >مشهد</Link>
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 sm:col-span-1" >کرج</Link>
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 sm:col-span-1" >شیراز</Link>
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 sm:col-span-1" >اصفهان</Link>
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 sm:col-span-1" >اهواز</Link>
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 sm:col-span-1" >تبریز</Link>
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 sm:col-span-1" >کرمانشاه</Link>
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 sm:col-span-1" >قم</Link>
-          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 font-medium col-span-2 col-start-3 sm:col-span-1" >رشت</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 sm:col-span-1" >تهران</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 sm:col-span-1" >مشهد</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 sm:col-span-1" >کرج</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 sm:col-span-1" >شیراز</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 sm:col-span-1" >اصفهان</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 sm:col-span-1" >اهواز</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 sm:col-span-1" >تبریز</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 sm:col-span-1" >کرمانشاه</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 sm:col-span-1" >قم</Link>
+          <Link to={'/posts'} className="text-center transition-colors hover:text-gray-950 dark:hover:text-white font-medium col-span-2 col-start-3 sm:col-span-1" >رشت</Link>
         </div>
       </div>
 
@@ -77,10 +73,10 @@ export default function Home() {
 
       {/* socials */}
       <div className="flex gap-10">
-        <IoLogoTwitter className="transition-colors hover:text-gray-950 cursor-pointer" size={18} />
-        <RiInstagramFill className="transition-colors hover:text-gray-950 cursor-pointer" size={18} />
-        <FaLinkedin className="transition-colors hover:text-gray-950 cursor-pointer" size={18} />
-        <SiAparat className="transition-colors hover:text-gray-950 cursor-pointer" size={18} />
+        <IoLogoTwitter className="transition-colors hover:text-gray-950 dark:hover:text-white cursor-pointer" size={18} />
+        <RiInstagramFill className="transition-colors hover:text-gray-950 dark:hover:text-white cursor-pointer" size={18} />
+        <FaLinkedin className="transition-colors hover:text-gray-950 dark:hover:text-white cursor-pointer" size={18} />
+        <SiAparat className="transition-colors hover:text-gray-950 dark:hover:text-white cursor-pointer" size={18} />
       </div>
 
 
