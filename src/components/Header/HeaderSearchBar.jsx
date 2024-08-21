0967e1a2a4d785e84edb15bd0c46cc6aa54840b4
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-21 14:32:36 +0330

    improve header input styles

--- src/components/Header/HeaderSearchBar.jsx
+++ src/components/Header/HeaderSearchBar.jsx
@@ -1,12 +1,16 @@
+import { BsSearch } from "react-icons/bs";
 import { CgSearch } from "react-icons/cg";
 
 
 
 export default function HeaderSearchBar() {
     return (
+
+
         <div className="relative w-full">
-            <input className="outline-none bg-transparent md:dark:bg-[#222] md:bg-gray-200 p-2 dark:border-gray-500 border-y-2 border-r-2 md:border-2 rounded-r-lg md:rounded-lg w-full" placeholder="جستجو در همهٔ آگهی‌ها" type="text" />
-            <CgSearch className="absolute left-3 top-0 bottom-0 my-auto" size={20} />
+            <input className=" outline-none dark:bg-transparent dark:border-gray-500 md:focus:border-main transition-colors border-gray-200 border-y-2 border-r-2 md:border-2 rounded-r-lg md:rounded-lg py-2 pl-3 pr-10 w-full" placeholder="جستجو در همهٔ آگهی‌ها" type="text" />
+            <BsSearch className="absolute right-2 top-0 bottom-0 my-auto" size={20} />
         </div>
+
     )
 }
