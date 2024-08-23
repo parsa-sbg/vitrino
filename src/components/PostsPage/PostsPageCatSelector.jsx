7b0604cd2b77987a9891e76b610dfe7e4b9174a0
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-23 15:18:00 +0330

    get and show all categories from server

--- src/components/PostsPage/PostsPageCatSelector.jsx
+++ src/components/PostsPage/PostsPageCatSelector.jsx
@@ -1,74 +1,16 @@
 import { memo, useState } from "react"
 import { FaAngleLeft } from "react-icons/fa6";
+import {useCategory} from '../../hooks/useCategory'
 
-const allCats = [
-    {
-        id: 1,
-        name: 'املاک',
-        subCats: [
-            {
-                id: 10,
-                name: 'فروش مسکونی',
-            },
-
-            {
-                id: 11,
-                name: 'اجارهٔ مسکونی',
-                subCats: [
-                    {
-                        id: 100205165,
-                        name: 'آپارتمان'
-                    },
-                    {
-                        id: 100205184,
-                        name: 'خانه و ویلا'
-                    }
-                ]
-            },
-            {
-                id: 30,
-                name: 'فروش اداری و تجاری',
-            }
-        ]
-    },
-    {
-        id: 2,
-        name: 'وسایل نقلیه',
-        subCats: [
-            {
-                id: 13,
-                name: 'خودرو',
-                subCats: [
-                    {
-                        id: 100,
-                        name: 'سواری و وانت'
-                    }
-                ]
-            }
-        ]
-    },
-    {
-        id: 1000,
-        name: 'کالای دیجیتال'
-    },
-    {
-        id: 1001,
-        name: 'خانه و آشپزخانه'
-    },
-    {
-        id: 1002,
-        name: 'خدمات'
-    },
-    {
-        id: 1003,
-        name: 'وسایل شخصی',
-    }
-]
 
 export default memo(function PostsPageCatSelector() {
 
     const [openCatIds, setOpenCatIds] = useState([])
     const [selectedCatId, setSelectedCatId] = useState(null)
+
+    const {allCats} = useCategory()
+    console.log(allCats);
+    
 
     const toggleCat = (catId, parentId) => {
         if (openCatIds.includes(catId)) {
@@ -83,11 +25,11 @@ export default memo(function PostsPageCatSelector() {
     const subSubCatGenerator = (subSubCats, parentId) => (
         <ul className={`border-r-2 pr-1 mr-5 transition-all duration-300 max-h-0 overflow-hidden ${openCatIds.includes(parentId) && 'max-h-64 my-1'}`}>
             {subSubCats.map(subSubCat => (
-                <li key={subSubCat.id}>
+                <li key={subSubCat._id}>
                     <span
-                        className={`${selectedCatId == subSubCat.id && 'text-main bg-gray-100 dark:bg-gray-700'} transition-colors duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg py-1 px-2 cursor-pointer block`}
-                        onClick={() => { setSelectedCatId(subSubCat.id) }}>
-                        {subSubCat.name}
+                        className={`${selectedCatId == subSubCat._id && 'text-main bg-gray-100 dark:bg-gray-700'} transition-colors duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg py-1 px-2 cursor-pointer block`}
+                        onClick={() => { setSelectedCatId(subSubCat._id) }}>
+                        {subSubCat.title}
                     </span>
                 </li>
             ))}
@@ -97,13 +39,13 @@ export default memo(function PostsPageCatSelector() {
     const subCatGenerator = (subCats, parentId) => (
         <ul className={`mr-5 flex flex-col gap-1 transition-all duration-300 max-h-0 overflow-hidden ${openCatIds.includes(parentId) && 'max-h-64 my-1'}`}>
             {subCats.map(subCat => (
-                <li key={subCat.id}>
+                <li key={subCat._id}>
                     <span
-                        className={`${selectedCatId == subCat.id && 'text-main bg-gray-100 dark:bg-gray-700'} transition-colors duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg py-1 px-2 flex items-center justify-between cursor-pointer`}
-                        onClick={() => { toggleCat(subCat.id, parentId) }}>
-                        {subCat.name} {subCat.subCats && <FaAngleLeft className={`transition-all duration-300 ${openCatIds.includes(subCat.id) && '-rotate-90'}`} size={12} />}
+                        className={`${selectedCatId == subCat._id && 'text-main bg-gray-100 dark:bg-gray-700'} transition-colors duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg py-1 px-2 flex items-center justify-between cursor-pointer`}
+                        onClick={() => { toggleCat(subCat._id, parentId) }}>
+                        {subCat.title} {subCat.subCategories && <FaAngleLeft className={`transition-all duration-300 ${openCatIds.includes(subCat._id) && '-rotate-90'}`} size={12} />}
                     </span>
-                    {subCat.subCats && subSubCatGenerator(subCat.subCats, subCat.id)}
+                    {subCat.subCategories && subSubCatGenerator(subCat.subCategories, subCat._id)}
                 </li>
             ))}
         </ul>
@@ -125,13 +67,13 @@ export default memo(function PostsPageCatSelector() {
                     همه آگهی ها
                 </span>
                 {allCats.map(cat => (
-                    <li key={cat.id}>
+                    <li key={cat._id}>
                         <span
-                            className={`${selectedCatId == cat.id && 'text-main bg-gray-100 dark:bg-gray-700 '} transition-colors duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg py-1 px-2 flex items-center justify-between cursor-pointer`}
-                            onClick={() => { toggleCat(cat.id) }}>
-                            {cat.name} {cat.subCats && <FaAngleLeft className={`${openCatIds.includes(cat.id) && '-rotate-90'} transition-all duration-300`} size={16} />}
+                            className={`${selectedCatId == cat._id && 'text-main bg-gray-100 dark:bg-gray-700 '} transition-colors duration-75 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg py-1 px-2 flex items-center justify-between cursor-pointer`}
+                            onClick={() => { toggleCat(cat._id) }}>
+                            {cat.title} {cat.subCategories && <FaAngleLeft className={`${openCatIds.includes(cat._id) && '-rotate-90'} transition-all duration-300`} size={16} />}
                         </span>
-                        {cat.subCats && subCatGenerator(cat.subCats, cat.id)}
+                        {cat.subCategories && subCatGenerator(cat.subCategories, cat._id)}
                     </li>
                 ))}
             </ul>
--- src/contexts/categoryContext.jsx
+++ src/contexts/categoryContext.jsx
@@ -1,14 +1,33 @@
 import propTypes from 'prop-types'
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+
 
 export const CategoryContext = createContext()
 
 export default function CategoryProvider({ children }) {
 
+    const [allCats, setAllCats] = useState([])
 
+    useEffect(() => {
+
+        fetch(`${baseUrl}/v1/category`)
+            .then(res => res.json())
+            .then(response => {
+
+                const allCats = response.data.categories
+                setAllCats(allCats)
+
+            }).catch(err => {
+                console.error('Fetch error:', err);
+            })
+
+    }, [])
     return (
         <CategoryContext.Provider
-            value={'test value'}>
+            value={{
+                allCats,
+            }}>
             {children}
         </CategoryContext.Provider>
     )
