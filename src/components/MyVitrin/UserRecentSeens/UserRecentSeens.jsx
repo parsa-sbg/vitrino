cc5bbbf2e8809ce61f38e5b18e68b53760a4b151
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-27 13:17:54 +0330

    hide bottom nav in myvitrin subs

--- src/components/MyVitrin/UserRecentSeens/UserRecentSeens.jsx
+++ src/components/MyVitrin/UserRecentSeens/UserRecentSeens.jsx
@@ -3,10 +3,32 @@ import RecentSeenBox from "./RecentSeenBox";
 
 export default function UserRecentSeens() {
   return (
-    <div className="fixed py-7 container md:max-w-full md:p-0 h-full md:static right-0 left-0 top-[48px] bottom-[60px] visible">
+    <div className="fixed overflow-y-scroll h-full md:max-h-[calc(100vh-100px)] custom-scrollbar py-7 container md:max-w-full md:p-0 md:static right-0 left-0 top-[48px] bottom-[60px] visible">
       <MobileMyVitrinHeader backButton title={'بازدیدهای اخیر'} />
 
-      <div className=" h-fit grid grid-cols-6 gap-3">
+      <div className=" grid md:pl-3 grid-cols-6 gap-3">
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
+        <RecentSeenBox />
         <RecentSeenBox />
         <RecentSeenBox />
         <RecentSeenBox />
