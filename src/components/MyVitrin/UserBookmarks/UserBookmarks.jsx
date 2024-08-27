2bdb51f5858469983529b46cf0d9db9bb7bf74ed
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-27 11:28:19 +0330

    add container class to myvitrin sub routes

--- src/components/MyVitrin/UserBookmarks/UserBookmarks.jsx
+++ src/components/MyVitrin/UserBookmarks/UserBookmarks.jsx
@@ -2,7 +2,7 @@ import MobileMyVitrinHeader from "../MobileMyVitrinHeader";
 
 export default function UserBookmarks() {
   return (
-    <div className="fixed md:static right-0 left-0 top-[48px] bottom-[60px] visible">
+    <div className="fixed container md:max-w-full md:p-0 h-full md:static right-0 left-0 top-[48px] bottom-[60px] visible">
       <MobileMyVitrinHeader backButton title={'نشان ها'} />
 
       <div className="bg-blue-300 h-full">
--- src/components/MyVitrin/UserPosts/UserPosts.jsx
+++ src/components/MyVitrin/UserPosts/UserPosts.jsx
@@ -2,7 +2,7 @@ import MobileMyVitrinHeader from "../MobileMyVitrinHeader";
 
 export default function UserPosts() {
   return (
-    <div className="fixed md:static right-0 left-0 top-[48px] bottom-[60px] visible">
+    <div className="fixed container md:max-w-full md:p-0 h-full md:static right-0 left-0 top-[48px] bottom-[60px] visible">
       <MobileMyVitrinHeader backButton title={'آگهی‌های من'} />
 
       <div className="bg-green-300 h-full">
