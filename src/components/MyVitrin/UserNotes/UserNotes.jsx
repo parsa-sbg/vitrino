bdfc8bf4700d2a5a4cb1bf55e17958b0ed91c045
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-08-27 14:07:22 +0330

    coding user notes component

--- src/components/MyVitrin/UserNotes/UserNotes.jsx
+++ src/components/MyVitrin/UserNotes/UserNotes.jsx
@@ -1,12 +1,23 @@
 import MobileMyVitrinHeader from "../MobileMyVitrinHeader";
+import UserNoteBox from "./UserNoteBox";
 
 export default function UserNotes() {
   return (
-    <div className="fixed container md:max-w-full md:p-0 h-full md:static right-0 left-0 top-[48px] bottom-[60px] visible">
+    <div className="fixed overflow-y-scroll h-full md:max-h-[calc(100vh-100px)] custom-scrollbar py-7 container md:max-w-full md:p-0 md:static right-0 left-0 top-[48px] bottom-[60px] visible">
       <MobileMyVitrinHeader backButton title={'یادداشت ها'} />
 
-      <div className="bg-yellow-300 h-full">
-        UserNotes
+      <div className=" md:pl-3 grid grid-cols-6 gap-3">
+        <UserNoteBox />
+        <UserNoteBox />
+        <UserNoteBox />
+        <UserNoteBox />
+        <UserNoteBox />
+        <UserNoteBox />
+        <UserNoteBox />
+        <UserNoteBox />
+        <UserNoteBox />
+        <UserNoteBox />
+        <UserNoteBox />
       </div>
 
     </div>
