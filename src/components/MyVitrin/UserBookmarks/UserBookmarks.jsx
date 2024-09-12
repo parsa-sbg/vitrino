9b678152fa43f68cf06ee72c3d4b016edcf68f2b
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-09-12 12:13:26 +0330

    fix my vitrin pages bug

--- src/components/MyVitrin/UserBookmarks/UserBookmarks.jsx
+++ src/components/MyVitrin/UserBookmarks/UserBookmarks.jsx
@@ -36,26 +36,31 @@ export default function UserBookmarks() {
   }
 
 
-  if (!userBookMarks.length) return (
-    <div className="col-span-12 md:col-span-8 overflow-y-scroll max-h-[calc(100vh-150px)] md:max-h-[calc(100vh-100px)] -ml-4 pl-4 h-full custom-scrollbar lg:col-span-9 grid xl:col-span-9 grid-cols-6 sm:gap-4 md:gap-0 lg:gap-4">
-      <div className="flex flex-col items-center justify-center w-full h-full col-span-12">
-        <NotFoundVector />
-        <div className="h-full text-center felx flex-col gap-2">
-          <span className="block">هنوز نشانی ندارید.</span>
-        </div>
-      </div>
-    </div>
-  )
-
   return (
-    <div className="fixed overflow-y-scroll h-full md:max-h-[calc(100vh-100px)] custom-scrollbar py-7 container md:max-w-full md:p-0 md:static right-0 left-0 top-[48px] bottom-[60px] visible">
+    <div className="fixed overflow-y-scroll overflow-x-hidden h-full md:max-h-[calc(100vh-100px)] custom-scrollbar py-7 container md:max-w-full md:p-0 md:static right-0 left-0 top-[48px] bottom-[60px] visible">
       <MobileMyVitrinHeader backButton title={'نشان ها'} />
 
-      <div className="grid grid-cols-6 md:pl-3 gap-3">
-        {userBookMarks.map(item => (
-          <UserBookmarkBox showModal={showModal} bookMarkItem={item} key={item._id} />
-        ))}
-      </div>
+      {
+        userBookMarks.length
+          ? (
+            <div className="grid grid-cols-6 md:pl-3 gap-3">
+              {userBookMarks.map(item => (
+                <UserBookmarkBox showModal={showModal} bookMarkItem={item} key={item._id} />
+              ))}
+            </div>
+          )
+          : (
+            <div className="col-span-12 md:col-span-8 overflow-y-scroll max-h-[calc(100vh-150px)] md:max-h-[calc(100vh-100px)] -ml-4 pl-4 h-full custom-scrollbar lg:col-span-9 grid xl:col-span-9 grid-cols-6 sm:gap-4 md:gap-0 lg:gap-4">
+              <div className="flex flex-col items-center justify-center w-full h-full col-span-12">
+                <NotFoundVector />
+                <div className="h-full text-center felx flex-col gap-2">
+                  <span className="block">هنوز نشانی ندارید.</span>
+                </div>
+              </div>
+            </div>
+          )
+      }
+
 
       <DeleteBookMarkModal getDataFromServer={getDataFromServer} bookMarkId={deleteBookMarkId} hideDeleteModal={hideModal} isModalShow={isModalShow} />
 
--- src/components/MyVitrin/UserPosts/UserPosts.jsx
+++ src/components/MyVitrin/UserPosts/UserPosts.jsx
@@ -34,26 +34,32 @@ export default function UserPosts() {
   }, [getUserPostsfromServer])
 
 
-  if (!userPosts.length) return (
-    <div className="col-span-12 md:col-span-8 overflow-y-scroll max-h-[calc(100vh-150px)] md:max-h-[calc(100vh-100px)] -ml-4 pl-4 h-full custom-scrollbar lg:col-span-9 grid xl:col-span-9 grid-cols-6 sm:gap-4 md:gap-0 lg:gap-4">
-      <div className="flex flex-col items-center justify-center w-full h-full col-span-12">
-        <NotFoundVector />
-        <div className="h-full text-center felx flex-col gap-2">
-          <span className="block">هنوز پستی ندارید.</span>
-        </div>
-      </div>
-    </div>
-  )
-
   return (
-    <div className="fixed overflow-y-scroll h-full md:max-h-[calc(100vh-100px)] custom-scrollbar py-7 container md:max-w-full md:p-0 md:static right-0 left-0 top-[48px] bottom-[60px] visible">
+    <div className="fixed overflow-y-scroll overflow-x-hidden h-full md:max-h-[calc(100vh-100px)] custom-scrollbar py-7 container md:max-w-full md:p-0 md:static right-0 left-0 top-[48px] bottom-[60px] visible">
       <MobileMyVitrinHeader backButton title={'آگهی‌های من'} />
 
-      <div className=" grid md:pl-3 grid-cols-6 gap-3">
-        {userPosts.map(post => (
-          <UserPostBox showDeleteModal={showDeleteModal} post={post} key={post._id} />
-        ))}
-      </div>
+
+      {
+        userPosts.length
+          ? (
+            <div className="w-full grid md:pl-3 grid-cols-6 gap-3">
+              {userPosts.map(post => (
+                <UserPostBox showDeleteModal={showDeleteModal} post={post} key={post._id} />
+              ))}
+            </div>
+          )
+          : (
+            <div className="col-span-12 md:col-span-8 overflow-y-scroll max-h-[calc(100vh-150px)] md:max-h-[calc(100vh-100px)] -ml-4 pl-4 h-full custom-scrollbar lg:col-span-9 grid xl:col-span-9 grid-cols-6 sm:gap-4 md:gap-0 lg:gap-4">
+              <div className="flex flex-col items-center justify-center w-full h-full col-span-12">
+                <NotFoundVector />
+                <div className="h-full text-center felx flex-col gap-2">
+                  <span className="block">هنوز پستی ندارید.</span>
+                </div>
+              </div>
+            </div>
+          )
+      }
+
 
       <DeletePostModal hideDeleteModal={hideDeleteModal} postId={deleteNoteId} isModalShow={isDeleteModalShow} getUserPostsfromServer={getUserPostsfromServer} />
 
