d8ab89a91d671cceacc4649e1bf93fe57584a374
Y********parsa-sbg <parsa.sbg@gmail.com>
2024-09-16 00:21:20 +0330

    fix header input bug

--- src/components/Header/HeaderSearchBar.jsx
+++ src/components/Header/HeaderSearchBar.jsx
@@ -3,12 +3,13 @@ import { useFilters } from "../../hooks/useFilters";
 
 export default function HeaderSearchBar() {
 
-    const { setFilter } = useFilters()
+    const { setFilter, filters } = useFilters()
 
     return (
 
         <div className="relative w-full">
             <input
+                value={filters.search}
                 onChange={e => { setFilter('search', e.target.value.trim()) }}
                 className=" outline-none dark:bg-transparent dark:border-gray-500 md:focus:border-main transition-colors border-gray-200 border-y-2 border-r-2 md:border-2 rounded-r-lg md:rounded-lg py-2 pl-3 pr-10 w-full"
                 placeholder="جستجو در آگهی‌ها" type="text" />
